14d48f26e69098a2458474c2949e74dbe6c6c2b3
Wavemaker Cards Novel Writing Software <59916864+wavemakercards@users.noreply.github.com>
2022-11-14 08:54:11 +0000

    deploy v4 alpha to github
    Now changing the package version will result in  deployment to the github.io repo

--- dev/src/tools/settings/Settings.vue
+++
@@ -1,5 +0,0 @@
-<template>
- <v-main>
-    <h1>Coming Soon</h1>
-     </v-main>
-</template>
--- v2/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create("window.html", {
-    "bounds": {
-        "width": screen.width,
-        "height": screen.height
-    },
-    'resizable':true
-    });
-});
